39a5c790839368577e61f81628eec89a5b526fd0
PNGLoc <PNGLoc@DESKTOP-7D8UCIQ>
2025-02-28 16:15:43 +0700

    Revert "add notification lỗi"
    This reverts commit 0d0f68df9100aa5a4195013545be93e2846cfb00.

--- SWP/src/main/java/Model/Account.java
+++ SWP/src/main/java/Model/Account.java
@@ -16,20 +16,20 @@ public class Account {
     private String UserName;
     private String UserRole;
     private String IdentityCard;
-    private String UserAddress;
+    private String AccountAddress;
     private String UserImage;
 
     public Account() {
     }
 
-    public Account(int UserId, String UserEmail, String UserPassword, String UserName, String UserRole, String IdentityCard, String UserAddress, String UserImage) {
+    public Account(int UserId, String UserEmail, String UserPassword, String UserName, String UserRole, String IdentityCard, String AccountAddress, String UserImage) {
         this.UserId = UserId;
         this.UserEmail = UserEmail;
         this.UserPassword = UserPassword;
         this.UserName = UserName;
         this.UserRole = UserRole;
         this.IdentityCard = IdentityCard;
-        this.UserAddress = UserAddress;
+        this.AccountAddress = AccountAddress;
         this.UserImage = UserImage;
     }
 
@@ -82,11 +82,11 @@ public class Account {
     }
 
     public String getAccountAddress() {
-        return UserAddress;
+        return AccountAddress;
     }
 
     public void setAccountAddress(String AccountAddress) {
-        this.UserAddress = AccountAddress;
+        this.AccountAddress = AccountAddress;
     }
 
     public String getUserImage() {
